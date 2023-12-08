f7a564115092cd30f160e1158b15a61585c74874
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2023-12-08 11:59:38 +0400

    fix: set space settings for terraform

--- lua/core/filetype.lua
+++ lua/core/filetype.lua
@@ -1,10 +1,9 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "helm",
+  pattern = "*.tf",
   callback = function(_)
     vim.opt.tabstop = 2
     vim.opt.shiftwidth = 2
     vim.opt.softtabstop = 2
-    vim.cmd [[ LspStop ]]
   end,
 })
 
