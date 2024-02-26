59e340119eec518155f9c6c8ed90b5cf8635f005
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2024-02-26 08:45:53 +0400

    chore: some updates

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -22,7 +22,7 @@ return require('packer').startup(function(use)
     'kyazdani42/nvim-tree.lua',
   }
   use 'glepnir/galaxyline.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
   use {
     'lukas-reineke/indent-blankline.nvim',
     tag = "v2.20.8",
