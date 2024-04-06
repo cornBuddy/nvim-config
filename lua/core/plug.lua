91e72018c08e31493b49a9ea29b10c19dc86e452
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2024-04-06 12:51:27 +0400

    fix: remove dap

--- lua/core/plug.lua
+++ lua/core/plug.lua
@@ -36,7 +36,4 @@ Plug('neovim/nvim-lspconfig')
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim')
 
-Plug('rcarriga/nvim-dap-ui')
-Plug('mfussenegger/nvim-dap')
-
 vim.call('plug#end')
